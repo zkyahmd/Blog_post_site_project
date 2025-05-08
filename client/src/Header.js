40f079cb122a434d6bf3ffe49bdbd086a57c8009
zkyahmd <zakyahamed978@gmail.com>
2025-05-08 12:22:44 +0530

    Changed header section

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,9 +1,13 @@
 import { Link,useNavigate } from "react-router-dom";
 import {useContext,useEffect, useState} from "react";
 import {UserContext} from "./UserContext";
+//import defaultAvatar from './defaultAvatar.png';
+//import defaultAvatar from './images/defaultAvatar.jpg';
 
 export default function Header() {
   const {setUserInfo,userInfo} = useContext(UserContext);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const defaultAvatar = process.env.PUBLIC_URL + "/images/defaultAvatar.jpg";
   const navigate = useNavigate(); 
   useEffect(() => {
     fetch('http://localhost:4000/profile', {
@@ -31,8 +35,17 @@ fetch('http://localhost:4000/logout',{
               <nav>
                 {username && (
                   <>
+                  <Link to="/" className="home">Home</Link>
                     <Link to="/create" className="newPost">Create new post</Link>
-                    <a onClick={logout} className="logout">Logout</a>
+                    <div className="profile-menu-wrapper" onClick={() => setMenuOpen(!menuOpen)}>
+                    <img src={defaultAvatar} alt="Profile" className="avatar" />
+              {menuOpen && (
+                <div className="dropdown">
+                  <Link to="/profile">Profile</Link>
+                  <button onClick={logout}>Logout</button>
+                </div>
+              )}
+            </div>
                   </>
                 )}
                 {!username && (
