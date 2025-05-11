6bbb63fbfb26fda39801dec2b8c92573f658f542
zkyahmd <zakyahamed978@gmail.com>
2025-05-11 20:39:45 +0530

    Scroll view added to quill editor

--- client/src/Editor.js
+++ client/src/Editor.js
@@ -1,6 +1,6 @@
 import ReactQuill from "react-quill";
 
-export default function Editor({value,onChange}) {
+export default function Editor({ value, onChange }) {
   const modules = {
     toolbar: [
       [{ header: [1, 2, false] }],
@@ -16,12 +16,13 @@ export default function Editor({value,onChange}) {
     ],
   };
   return (
-    <div className="content">
-    <ReactQuill
-      value={value}
-      theme={'snow'}
-      onChange={onChange}
-      modules={modules} />
+    <div className="quill-editor">
+      <ReactQuill
+        value={value}
+        theme="snow"
+        onChange={onChange}
+        modules={modules}
+      />
     </div>
   );
 }
